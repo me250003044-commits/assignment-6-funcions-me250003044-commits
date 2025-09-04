eb218343a24d3ea7ce96acdcb2a148894f183c7d
me250003044-commits <me250003044@iiti.ac.in>
2025-09-04 10:24:17 +0530

    Update gcd.cpp

--- gcd.cpp
+++ gcd.cpp
@@ -4,7 +4,11 @@ using namespace std;
 // Recursive function to find GCD of two numbers
 int gcd(int a, int b) {
     // TODO: complete the function as per instructions
-    
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+   
 }
 
 int main() {
@@ -15,4 +19,3 @@ int main() {
 
     return 0;
 }
-
