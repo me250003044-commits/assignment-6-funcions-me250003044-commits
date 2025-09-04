4cb9e28e8796bb5c785c17df23a1f94cde84cb34
me250003044-commits <me250003044@iiti.ac.in>
2025-09-04 10:31:08 +0530

    Update prime_palindrome.cpp

--- prime_palindrome.cpp
+++ prime_palindrome.cpp
@@ -3,17 +3,29 @@ using namespace std;
 
 bool isPrime(int n) {
     // TODO: complete the function as per instructions
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
 
 }
 
 bool isPalindrome(int n) {
     // TODO: complete the function as per instructions
+    int original = n, reversed = 0;
+    while (n > 0) {
+        reversed = reversed * 10 + n % 10;
+        n /= 10;
+    }
+    return original == reversed;
 
 }
 
 bool primePalindrome(int n) {
     // TODO: complete the function as per instructions
-    
+    return isPrime(n) && isPalindrome(n);
+   
 }
 
 int main() {
@@ -21,4 +33,3 @@ int main() {
     cout << (primePalindrome(n) ? 1 : 0) << "\n";
     return 0;
 }
-
